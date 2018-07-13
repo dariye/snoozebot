3615a0c47037f1f0eb29d287b9dbebdd774991a3
Paul Dariye <paul.dariye@gmail.com>
2018-07-12 20:27:26 -0400

    Add message formatting and set default snooze time to 2hrs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { send, json } = require('micro')
 const { router, get, post }  = require('microrouter')
 const { WebClient } = require('@slack/client')
 const microAuthSlack = require('microauth-slack')
+const moment = require('moment')
 
 /**
  * TODO:
@@ -54,29 +55,69 @@ const auth = async (req, res, auth) => {
   return send(res, 200, '<p>Nudge was successfully installed on your team.</p>')
 }
 
+const channelInfo = async (channel) => {
+  const [err, data] = await to(web.channels.info({ channel }))
+  if (err) throw new Error(err)
+  return data.channel
+}
+
+const groupInfo = async (channel) => {
+  const [err, data] = await to(web.groups.info({ channel }))
+  if (err) throw new Error(err)
+  return data.group
+}
+
+const findChannel = async (id) => {
+  if (/^[G]+/g.test(id)) return await groupInfo(id)
+  if (/^[C]+/g.test(id)) return await channelInfo(id)
+}
+
+const findEvent = async (channel, latest) => {
+  let err, event
+  if (/^[G]+/g.test(channel)) {
+    [err, event] = await to(web.groups.history({ channel, latest, inclusive: true, count: 1 }))
+  }
+  if (/^[C]+/g.test(channel)) {
+    [err, event] = await to(web.channels.history({ channel, latest, inclusive: true, count: 1 }))
+  }
+  if (err) throw new Error(err)
+  return event
+}
+
+const findUser = async (user) => {
+  const [err, data] = await to(web.users.info({ user }))
+  if (err) throw new Error(err)
+  return data.user
+}
+
 const addReminder = async ({
-  type, user, item, reaction: reacted, event_ts
+  type, user, item, reaction, item_user, event_ts
 }) => {
-  if (reacted !== reaction) return
-  const now = moment()
-  const text = `You asked me to remind you to get back to something`
-  const time = `in one hour`
+  const channel = await findChannel(item.channel)
+  const { messages } = await findEvent(channel.id, item.ts)
+  const text = `About <https://andela.slack.com/archives/${channel.id}/p${item.ts.replace('.', '')}|this message> from <@${item_user}> in ${channel.name}`
+  const time = `in 2 hours`
   const [err, reminder] = await to(web.reminders.add({ text, user, time }))
   if (err) throw new Erro(err)
-  return reminder
+  return { ...reminder, message: messages[0] }
 }
 
 const whisper = async ({
   ok,
-  reminder: { user },
-  event: { item: { channel } }
+  reminder: { user, time },
+  message: { text: messageText },
+  event: { item: { channel, ts }, item_user }
 }) => {
   if (!ok) return
-  const text = `I'll remind you in an hour to come back to this`
+  const now = moment()
+  const snoozeTime = moment(time)
+  const text = `:thumbsup_all: I will remind you to come back to <https://andela.slack.com/archives/${channel}/p${ts.replace('.', '')}|this message> from ${item_user === user ? 'you' : '<@' + item_user + '>' }\n(${messageText.substr(1, 50)}) in 2 hours at ${snoozeTime.format('hh:mm')} ${snoozeTime.diff(now, 'days') > 0 ? 'tomorrow' : 'today'}.`
   const [err, message] = await to(web.chat.postEphemeral({ channel, text, user }))
   if (err) throw new Error(err)
   return message
 }
+
+
 
 const events = async (req, res) => {
   try {
@@ -84,12 +125,11 @@ const events = async (req, res) => {
     if (err) throw new Error(err)
     if (payload && verificationToken !== payload.token) throw new Error(`unauthorized`)
     const { type, event } = payload
-    if (type === 'url_verification') {
-      console.log(payload)
-      return send(res, 200, JSON.stringify(payload))
-    }
-    if (type === 'event_callback' && event.type === 'reaction_added') {
-      const reminder = await addReminder(payload.event)
+    if (type === 'url_verification') return send(res, 200, JSON.stringify(payload))
+    if (type !== 'event_callback' && event.reaction !== reaction) return send(res, 200)
+    if (!event.item_user) return
+    if (event.type === 'reaction_added') {
+      const reminder = await addReminder(event)
       const message = await whisper({ ...reminder, event })
     }
     return send(res, 200)
@@ -101,10 +141,10 @@ const events = async (req, res) => {
 
 const notfound = (req, res) => send(res, 404, 'Not found')
 module.exports = router(
-  get('/', index),
-  get('/auth/slack', slackAuth(auth)),
-  get('/auth/slack/callback', slackAuth(auth)),
+  // get('/', index),
+  // get('/auth/slack', slackAuth(auth)),
+  // get('/auth/slack/callback', slackAuth(auth)),
   post('/slack/events', events),
-  get('/*', notfound)
+  // get('#<{(|', notfound)
 )
 

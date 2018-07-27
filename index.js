5ae48921eee3787f9aae6b25458f219a127a1169
Paul Dariye <paul.dariye@gmail.com>
2018-07-27 16:05:34 -0400

    Refactor slack module

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const fs = require('fs')
+const path = require('path')
 const { send, json } = require('micro')
 const { router, get, post }  = require('microrouter')
-const { WebClient } = require('@slack/client')
-const microAuthSlack = require('microauth-slack')
-const moment = require('moment')
+const authSlack = require('microauth-slack')
+const rateLimit = require('micro-ratelimit')
+const handlebars = require('handlebars')
+const toPromise = require('denodeify')
+const { parse } = require('url')
+const mime = require('mime')
 
 /**
  * TODO:
@@ -24,11 +29,13 @@ const {
     reaction,
     scope,
     callbackUrl
-  }
+  },
+  common
 } = require('./config')
-const { to } = require('./utils')
 
-const web = new WebClient(token)
+const { to } = require('./utils')
+const Slack = require('./lib/slack')
+
 const options = {
   clientId,
   clientSecret,
@@ -37,87 +44,12 @@ const options = {
   path: '/auth/path'
 }
 
-const slackAuth = microAuthSlack(options)
-
-const index = (req, res) => {
-  return send(res, 200, `<a href="https://slack.com/oauth/authorize?client_id=289290819732.396355442160&scope=reactions:read,bot,reminders:write,reminders:read,chat:write:bot"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`)
-}
-
+const slackAuth = authSlack(options)
 const auth = async (req, res, auth) => {
-  if(!auth) {
-    return send(res, 404, 'Not Found')
-  }
-  if (auth.err) {
-    console.error(auth.err)
-    return send(res, 403, 'Forbidden')
-  }
-  console.log(auth)
-  return send(res, 200, '<p>Nudge was successfully installed on your team.</p>')
+  if(!auth) return send(res, 404, 'Not Found')
+  if (auth.err) return send(res, 403, new Error(auth.err))
+  return send(res, 200, '<p>Snoozebot was successfully installed on your team.</p>')
 }
-
-const channelInfo = async (channel) => {
-  const [err, data] = await to(web.channels.info({ channel }))
-  if (err) throw new Error(err)
-  return data.channel
-}
-
-const groupInfo = async (channel) => {
-  const [err, data] = await to(web.groups.info({ channel }))
-  if (err) throw new Error(err)
-  return data.group
-}
-
-const findChannel = async (id) => {
-  if (/^[G]+/g.test(id)) return await groupInfo(id)
-  if (/^[C]+/g.test(id)) return await channelInfo(id)
-}
-
-const findEvent = async (channel, latest) => {
-  let err, event
-  if (/^[G]+/g.test(channel)) {
-    [err, event] = await to(web.groups.history({ channel, latest, inclusive: true, count: 1 }))
-  }
-  if (/^[C]+/g.test(channel)) {
-    [err, event] = await to(web.channels.history({ channel, latest, inclusive: true, count: 1 }))
-  }
-  if (err) throw new Error(err)
-  return event
-}
-
-const findUser = async (user) => {
-  const [err, data] = await to(web.users.info({ user }))
-  if (err) throw new Error(err)
-  return data.user
-}
-
-const addReminder = async ({
-  type, user, item, reaction, item_user, event_ts
-}) => {
-  const channel = await findChannel(item.channel)
-  const { messages } = await findEvent(channel.id, item.ts)
-  const text = `About <https://andela.slack.com/archives/${channel.id}/p${item.ts.replace('.', '')}|this message> from <@${item_user}> in ${channel.name}`
-  const time = `in 2 hours`
-  const [err, reminder] = await to(web.reminders.add({ text, user, time }))
-  if (err) throw new Erro(err)
-  return { ...reminder, message: messages[0] }
-}
-
-const whisper = async ({
-  ok,
-  reminder: { user, time },
-  message: { text: messageText },
-  event: { item: { channel, ts }, item_user }
-}) => {
-  if (!ok) return
-  const now = moment()
-  const snoozeTime = moment(time)
-  const text = `:thumbsup_all: I will remind you to come back to <https://andela.slack.com/archives/${channel}/p${ts.replace('.', '')}|this message> from ${item_user === user ? 'you' : '<@' + item_user + '>' }\n(${messageText.substr(1, 50)}) in 2 hours at ${snoozeTime.format('hh:mm')} ${snoozeTime.diff(now, 'days') > 0 ? 'tomorrow' : 'today'}.`
-  const [err, message] = await to(web.chat.postEphemeral({ channel, text, user }))
-  if (err) throw new Error(err)
-  return message
-}
-
-
 
 const events = async (req, res) => {
   try {
@@ -126,11 +58,14 @@ const events = async (req, res) => {
     if (payload && verificationToken !== payload.token) throw new Error(`unauthorized`)
     const { type, event } = payload
     if (type === 'url_verification') return send(res, 200, JSON.stringify(payload))
-    if (type !== 'event_callback' && event.reaction !== reaction) return send(res, 200)
-    if (!event.item_user) return
+    if (type !== 'event_callback' || event.reaction !== reaction) return send(res, 200)
+    if (!event.item_user) return send(res, 200)
     if (event.type === 'reaction_added') {
-      const reminder = await addReminder(event)
-      const message = await whisper({ ...reminder, event })
+      const slack = new Slack(token, { event })
+      slack.channel = event.item.channel
+      slack.event = event.item.channel
+      const reminder = await slack.addReminder()
+      const message = await slack.whisper({ ...reminder, event })
     }
     return send(res, 200)
   } catch(err) {
@@ -139,12 +74,119 @@ const events = async (req, res) => {
   }
 }
 
-const notfound = (req, res) => send(res, 404, 'Not found')
+let cachedView = null
+const getView = async () => {
+  if (!cachedView) {
+    try {
+      const file = await toPromise(fs.readFile)(
+        path.resolve(__dirname, './views/index.hbs'),
+        'utf8'
+      )
+      cachedView = handlebars.compile(file)
+    } catch(err) {
+      console.log(err)
+      throw new Error(err)
+    }
+  }
+  return cachedView
+}
+
+let assets = {}
+const getAsset = async (assetPath) => {
+  if (!assets[assetPath]) {
+    try {
+      const file = await toPromise(fs.readFile)(
+        path.resolve(__dirname, './static', assetPath),
+        'utf8'
+      )
+      assets[assetPath] = file
+    } catch (err) {
+      console.log(err)
+      throw new Error(err)
+    }
+  }
+  return assets[assetPath]
+}
+
+const renderView = async (directory) => {
+  const data = {
+    assetsDir: '/static'
+  }
+  const view = await getView()
+  return view(data)
+}
+
+const renderImage = async (file) => {
+  try {
+    const content = await toPromise(fs.readFile)(path.resolve(process.cwd(), file))
+    return {
+      content,
+      mime: mime.getType(file)
+    }
+  } catch (err) {
+    console.log(err)
+    throw new Error(err)
+  }
+}
+
+const clientView = async (req, res) => {
+  const renderedView = await renderView()
+  return send(res, 200, renderedView)
+}
+
+const validExtensions = new Set([
+  '.jpeg',
+  '.jpg',
+  '.png',
+  '.gif',
+  '.bmp',
+  '.tiff',
+  '.tif',
+  '.ico'
+])
+
+const exists = async (filePath) => {
+  try {
+    await toPromise(fs.stat)(filePath)
+    return true
+  } catch (err) {
+    return false
+  }
+}
+
+const catchAll = async (req, res) => {
+  const { pathname } = parse(req.url)
+  const pathObj = path.parse(path.join(process.cwd(), pathname))
+  if (pathname.startsWith('/static/')) {
+    const asset = await getAsset(pathname.replace('/static/', ''))
+    res.setHeader('Content-Type', `${mime.getType(pathname)}; charset=utf-8`)
+    return send(res, 200, asset)
+  }
+
+  if (!awaits(reqPath)) {
+    return send(res, 404, 'Not found')
+  }
+
+  if (pathObj.ext === '') {
+    return send(res, 200)
+  } else if (validExtensions.has(pathObj.ext)) {
+    try {
+      const image = await renderImage(reqPath)
+      res.setHeader('Content-Type', `${image.mime}; charset=utf-8`)
+      return send(res, 200, image.content)
+    } catch (err) {
+      return send(res, 500, 'Error reading file content')
+    }
+  } else {
+    return send(res, 400, 'Bad request')
+  }
+}
+
 module.exports = router(
-  // get('/', index),
-  // get('/auth/slack', slackAuth(auth)),
-  // get('/auth/slack/callback', slackAuth(auth)),
+  get('/', clientView),
+  get('/auth/slack', slackAuth(auth)),
+  get('/auth/slack/callback', slackAuth(auth)),
   post('/slack/events', events),
-  // get('#<{(|', notfound)
+  get('/*', catchAll)
 )
 
